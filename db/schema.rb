ceacd731ff86de3bfb994c8aae6466240ac10b80
Mario Keophilalay <yanndyna@gmail.com>
2018-05-24 02:15:30 +0000

    updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520174838) do
+ActiveRecord::Schema.define(version: 20180520170626) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
