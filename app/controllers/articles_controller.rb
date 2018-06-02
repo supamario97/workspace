8990ba939b2c5a4c748c2c2f032187033a66f185
Mario Keophilalay <yanndyna@gmail.com>
2018-06-02 16:18:27 +0000

    Added new flash styles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Your Article Was Successfully Created"
+      flash[:success] = "Your Article Was Successfully Created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "Your Article Was Successfully Updated"
+      flash[:success] = "Your Article Was Successfully Updated"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
